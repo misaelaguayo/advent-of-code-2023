5cab3cb14fb7d8c05cfa0f36f59a41b5e8c88ea3
Misael Aguayo <aguayo.misael@gmail.com>
2023-12-06 08:37:25 -0600

    Day 6

---
+++ 6/p6-2.py
@@ -0,0 +1,29 @@
+def products(arr):
+    res = 1
+    for a in arr:
+        res *= a
+    return res
+lines = []
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+lines = [line.strip() for line in lines]
+lines = [line.split(":")[1:][0].strip() for line in lines]
+lines = [line.split(' ') for line in lines]
+lines = [list(filter(lambda x: x, line)) for line in lines]
+
+times, distances = lines
+times = [int("".join(times))]
+distances = [int("".join(distances))]
+
+res = []
+for t, time in enumerate(times):
+    curr = 0
+    for speed in range(time + 1):
+        currDistance = speed * (time - speed)
+        if currDistance > distances[t]:
+            curr += 1
+    res.append(curr)
+
+res = products(res)
+print(res)
+
