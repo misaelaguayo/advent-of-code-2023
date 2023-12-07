1c4310ed7aa095fa1264da9e23876cb78f5c74a2
Misael Aguayo <aguayo.misael@gmail.com>
2023-12-06 19:04:01 -0600

    Ugly 3 part 2

--- 3/p3-2.py
+++ 3/p3-2.py
@@ -6,46 +6,43 @@ def makeGrid():
             grid.append([c for c in line])
     return grid
 
-def hasSymbolAround(grid, r, c):
+def visitNumber(grid, r, c, seen):
     numbers = "0123456789"
-    offset = [0, 1, -1]
-    rows = len(grid)
-    columns = len(grid[0])
+    seen[r][c] = True
+    left, right = c, c
 
-    for i in offset:
-        for j in offset:
-            if r + i >= 0 and r + i < rows and c + j >= 0 and c + j < columns:
-                if grid[r+i][c+j] not in numbers and grid[r+i][c+j] != ".":
-                    return True
-    return False
+    while left >= 0 and grid[r][left] in numbers:
+        seen[r][left] = True
+        left -= 1
+    while right < len(grid[0]) and grid[r][right] in numbers:
+        seen[r][right] = True
+        right += 1
+    return int("".join(grid[r][left+1:right]))
 
-def findSchems(grid, r):
-    res = []
+def visit(grid, r, c):
     numbers = "0123456789"
-    currNum = ""
-    hasSymbol = False
-    string = grid[r]
-    for c in range(len(string)):
-        if string[c] not in numbers:
-            if currNum:
-                res.append((currNum, hasSymbol))
-            currNum = ""
-            hasSymbol = False
-        else:
-            currNum += string[c]
-            hasSymbol = hasSymbol or hasSymbolAround(grid, r, c)
-    if currNum:
-        res.append((currNum, hasSymbol))
-    return res
+    seen = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
+    seen[r][c] = True
+
+    moves = [1, 0, -1]
+    nums = []
+    for x in moves:
+        for y in moves:
+            if r + x >= 0 and r + x < len(grid) and c + y >= 0 and c + y < len(grid[0]) and not seen[r+x][c+y] and grid[r+x][c+y] in numbers:
+                nums.append(visitNumber(grid, r+x, c+y, seen))
+
+    return nums
 
 def main():
     grid = makeGrid()
+    r, c = len(grid), len(grid[0])
     res = 0
-
-    for row in range(len(grid)):
-        nums = findSchems(grid, row)
-        schems = list(filter(lambda x: x[1], nums))
-        res += sum([int(schem[0]) for schem in schems])
-
+    for row in range(r):
+        for column in range(c):
+            if grid[row][column] == "*":
+                v = visit(grid, row, column)
+                if len(v) == 2:
+                    res += v[0] * v[1]
     return res
 
+print(main())
